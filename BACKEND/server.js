3699e69b14047f7c895c6737298a3a94dd2e24f4
AliHussain <alihussain08950@gmail.com>
2024-08-17 19:54:12 +0300

    BASIC JWT JSON WEB TOKEN Authentication has been setup and is applied on protected routes through middleware

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -26,39 +26,49 @@ app.use(morgan('dev'));
 // Importing monogoDB connection
 const database = require('./config/database');
 
+// Importing jsonwebtoken
+const jwt = require('jsonwebtoken');
+
 // Importing models
 const User = require('./models/userModel');
 const Project = require('./models/projectModel');
 const Task = require('./models/taskModel');
 
-
 // setting up signup route
 app.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username: username });
+    const user = await User.findOne({ username: req.body.username });
     if (user) {
         return res.status(400).json({ message: 'User already exists' });
     }
-    const hashedPassword = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS));
-    await User.create({ username, password: hashedPassword });
+    const hashedPassword = await bcrypt.hash(req.body.password, parseInt(process.env.SALT_ROUNDS));
+    await User.create({ username: req.body.username, password: hashedPassword });
     res.status(201).json({ message: 'User created' });
 });
 
 // setting up sign in route
 app.post('/signin', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await
-        User.findOne({ username: username });
+    const user = await User.findOne({ username: req.body.username });
     if (!user) {
         return res.status(404).json({ message: 'User not found' });
     }
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
     if (!isMatch) {
         return res.status(400).json({ message: 'Invalid credentials' });
     }
-    res.status(200).json({ message: 'Sign in successful' });
+    const payload = {
+        id: user._id,
+        username: user.username
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+    res.status(200).json({ message: 'Sign in successful', token });
 }
 );
+
+// Token authentication middleware
+const authenticateToken = require('./middleware/authenticateToken');
+app.use(authenticateToken); 
+
+// Protected routes
 
 // Setting up port
 const PORT = process.env.PORT || 3000;
