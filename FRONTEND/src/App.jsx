bbd486971ef176383c0552423b82bbdc1f0b5ef3
AliHussain <alihussain08950@gmail.com>
2024-08-17 12:50:53 +0300

    Exported each component in its respected file and imported the components inside app.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+// Importing the Authentication Components
+import SignIn from './components/Authentication/signIn/signIn';
+import SignUp from './components/Authentication/signUp/signUp';
+
+// Importing the Project Components
+import ProjectForm from './components/ProjectComponents/projectForm/projectForm';
+import ProjectItem from './components/ProjectComponents/projectItem/projectItem';
+import ProjectList from './components/ProjectComponents/projectList/projectList';
+
+// Importing the Task Components
+import TaskForm from './components/TaskComponents/taskForm/taskForm';
+import TaskItem from './components/TaskComponents/taskItem/taskItem';
+import TaskList from './components/TaskComponents/taskList/taskList';
+
+// Importing the Semantic Components
+import Footer from './components/SemanticComponents/footer/footer';
+import Navbar from './components/SemanticComponents/navbar/navbar';
 
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <SignIn />
+      <SignUp />
+      <ProjectForm />
+      <ProjectItem />
+      <ProjectList />
+      <TaskForm />
+      <TaskItem />
+      <TaskList />
+      <Footer />
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ FRONTEND/src/components/TaskComponents/taskForm/taskForm.jsx
@@ -0,0 +1,7 @@
+export default function taskForm() {
+    return (
+      <div>
+        <h1>TaskForm</h1>
+      </div>
+    )
+  }
