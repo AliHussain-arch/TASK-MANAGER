fb87b8c45e406700513211288fecdf5a59f73732
Fadem300 <fadel.m200@gmail.com>
2024-08-18 14:33:27 +0300

    //i created a footer //i created project Form //i add package react-router-dom // I add node_modules to gitignore

--- FRONTEND/src/components/ProjectComponents/projectForm/projectForm.jsx
+++ FRONTEND/src/components/ProjectComponents/projectForm/projectForm.jsx
@@ -1,7 +1,14 @@
 export default function projectForm() { 
     return (
       <div>
-        <h1>ProjectForm</h1>
+        <form >
+        <div>
+            <label htmlFor="name">project Name</label>
+            <input type="text" id="name" name="name" placeholder="project Name" />
+          </div>
+          <button type="submit">Add project Name </button>
+
+        </form>
       </div>
     )
 }
--- FRONTEND/src/components/SemanticComponents/footer/footer.jsx
+++ FRONTEND/src/components/SemanticComponents/footer/footer.jsx
@@ -1,7 +1,28 @@
-export default function footer() { 
-    return (
-      <div>
-        <h1>Footer</h1>
-      </div>
-    )
-  }
+export default function footer() {
+  return (
+    <div>
+      <footer>
+        <div className="footer-content">
+          <h3></h3>
+          <ul className="footerHelp">
+            <li>
+              <a href="#">Contact Us</a>
+              <i className="fas fa-solid fa-phone"></i>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+              <i className="fas fa-regular fa-address-card"></i>
+            </li>
+            <li>
+              <a href="#">Help</a>
+              <i className="fas fa-question"></i>
+            </li>
+          </ul>
+          <div className="footer-bottom">
+            <p>Copyright 2024. Registration System. All Rights Reserved</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
