e831727b965f3f19f2539422866c8f1cd820af4a
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-18 14:17:09 +0300

    Created Task form

--- FRONTEND/src/components/TaskComponents/taskForm/taskForm.jsx
+++ FRONTEND/src/components/TaskComponents/taskForm/taskForm.jsx
@@ -1,7 +1,24 @@
 export default function taskForm() {
     return (
+      <form>
       <div>
         <h1>TaskForm</h1>
+        <input type="text" value={title}  />
       </div>
+
+      <div>
+        <label htmlFor="title">Title:</label>
+      </div>
+
+      <div>
+        <label htmlFor="description">Description:</label>
+      </div>
+
+       <div>
+        <button type="submit">Add Task</button>
+
+      </div>
+      </form>
     )
   }
+
