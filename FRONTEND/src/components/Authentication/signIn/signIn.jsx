93295d5b73c48d90d0b8e5a90114492bb7c5da8a
AliHussain <alihussain08950@gmail.com>
2024-08-18 12:35:31 +0300

    Created a sign in and sign up form

--- FRONTEND/src/components/Authentication/signIn/signIn.jsx
+++ FRONTEND/src/components/Authentication/signIn/signIn.jsx
@@ -2,6 +2,13 @@ export default function signIn() {
   return (
     <div>
       <h1>SignIn</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" placeholder="username" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="password" />
+        <button type="submit">Sign In</button>
+      </form>
     </div>
   )
 }
--- FRONTEND/src/components/Authentication/signUp/signUp.jsx
+++ FRONTEND/src/components/Authentication/signUp/signUp.jsx
@@ -2,6 +2,21 @@ export default function signUp() {
     return (
       <div>
         <h1>SignUp</h1>
+        <form>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="username" />
+          </div>
+          <div>
+            <label htmlFor="email">Password</label>
+            <input type="password" id="password" name="password" placeholder="password" />
+          </div>
+          <div>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input type="password" id="confirmPassword" name="confirmPassword" placeholder="confirm password" />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
       </div>
     )
   }
