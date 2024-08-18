4b9866f3a75d20659dde72b98af20209257162fc
AliHussain <alihussain08950@gmail.com>
2024-08-18 14:29:42 +0300

    refactored the taskform

--- FRONTEND/src/components/TaskComponents/taskForm/taskForm.jsx
+++ FRONTEND/src/components/TaskComponents/taskForm/taskForm.jsx
@@ -1,24 +1,21 @@
 export default function taskForm() {
     return (
-      <form>
-      <div>
+      <>
         <h1>TaskForm</h1>
-        <input type="text" value={title}  />
-      </div>
-
-      <div>
-        <label htmlFor="title">Title:</label>
-      </div>
-
-      <div>
-        <label htmlFor="description">Description:</label>
-      </div>
-
-       <div>
-        <button type="submit">Add Task</button>
-
-      </div>
-      </form>
+        <form>
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input id="title" name="title" placeholder="Title" />
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <textarea id="description" name="description" placeholder="Description" />
+          </div>
+          <div>
+            <button type="submit">Add Task</button>
+          </div>
+        </form>
+      </>
     )
   }
 
