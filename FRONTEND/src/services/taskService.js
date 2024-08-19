19d070ca3412b050676984c5ecb016bce94b1e91
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-19 12:44:08 +0300

    Add updateTask function to update tasks via PUT request

--- FRONTEND/src/services/taskService.js
+++ FRONTEND/src/services/taskService.js
@@ -33,8 +33,26 @@ const listTasks = async () => {
       throw error;
     }
   };
+  const updateTask = async () => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/:userId/projects/:projectId/tasks/:taskId`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const json = await res.json();
+      if (!res.ok) {
+        throw new Error(json.message || "Something went wrong");
+      }
+      return json;
+    } catch (error) {
+      console.error("Error", error);
+      throw error;
+    }
+  };
 
 export default {
   createTask,
   listTasks,
+  updateTask,
 };
