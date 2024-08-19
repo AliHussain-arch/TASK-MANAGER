b87d57549217a76430e83cf33e8fa2ff234661d9
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-19 12:49:45 +0300

    Add deleteTask function to remove tasks via DELETE request and export task functions

--- FRONTEND/src/services/taskService.js
+++ FRONTEND/src/services/taskService.js
@@ -50,9 +50,26 @@ const listTasks = async () => {
       throw error;
     }
   };
-
+  const deleteTask = async () => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/:userId/projects/:projectId/tasks/:taskId`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const json = await res.json();
+      if (!res.ok) {
+        throw new Error(json.message || "Something went wrong");
+      }
+      return json;
+    } catch (error) {
+      console.error("Error", error);
+      throw error;
+    }
+  };
 export default {
   createTask,
   listTasks,
   updateTask,
+  deleteTask
 };
