5f903b7cf4e5a23f6ab0f4e90f11b70e16b4b156
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-20 11:35:26 +0300

    work in 'BACKEND' Add arrow function in BACKEND/controllers/projectControllers.js

---
+++ BACKEND/controllers/projectControllers.js
@@ -0,0 +1,23 @@
+
+
+// Create project
+const CreateProject = function() {
+    return;
+}
+
+// List projects
+const ListProject = function() {
+    return;
+}
+
+// Update project
+const UpdateProject = function() {
+    return;
+}
+
+// Delete project
+const DeleteProject = function() {
+    return;
+}
+
+module.exports = {CreateProject,ListProject,UpdateProject,DeleteProject}
