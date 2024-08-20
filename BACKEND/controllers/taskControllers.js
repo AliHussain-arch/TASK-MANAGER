18427e1b64df0857e379bc2b9b94cabef35a911d
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-20 11:35:45 +0300

    work in 'BACKEND' Add arrow function in BACKEND/controllers/taskControllers.js

---
+++ BACKEND/controllers/taskControllers.js
@@ -0,0 +1,22 @@
+
+// Create task under a project
+const CreateTask = function() {
+    return;
+}
+
+// List tasks under a project
+const ListTasks = function() {
+    return;
+}
+
+// Update a single task
+const UpdateTask = function() {
+    return;
+}
+
+// Delete a single task
+const DeleteTask = function() {
+    return;
+}
+
+module.exports = {CreateTask,ListTasks,UpdateTask,DeleteTask}
