fe88946bc8141921ab2433c2a6cdb2a7b8ada763
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-21 12:50:40 +0300

    work in create About page in frontend

---
+++ FRONTEND/src/components/About/about.jsx
@@ -0,0 +1,10 @@
+import '../About/About.css'
+export default function About() {
+  return (
+    <section id="about-us">
+      <h2>About Us</h2>
+      <p>Welcome to our website. We are dedicated to providing the best service possible. Our team is passionate about what we do and we are always here to help you with any questions or concerns you may have.</p>
+    </section>
+
+  );
+};
