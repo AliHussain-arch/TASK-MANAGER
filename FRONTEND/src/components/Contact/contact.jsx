fc632bcd704bb15e14c1d434f331157d6d75ae0b
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-21 12:55:56 +0300

    work in create Contact page in frontend

---
+++ FRONTEND/src/components/Contact/contact.jsx
@@ -0,0 +1,11 @@
+import '../Contact/contact.css'
+export default function Contact() {
+    return (
+        <section id="contact-us">
+        <h2>Contact Us</h2>
+        <p>Email: <a href="mailto:support@CodeTrio.com">support@CodeTrio.com</a></p>
+        <p>GitHub: <a href="https://github.com/AliHussain-arch/TASK-MANAGER-GROUP-PROJECT" target="_blank">github TASK-MANAGER-GROUP-PROJECT</a></p>
+        </section>
+    
+    );
+}
